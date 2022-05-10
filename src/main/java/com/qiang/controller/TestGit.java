1d48b009964a3b5e147e698ad0735bd46abfa5e6
孙国强 <2191564427@qq.com>
2022-05-10 21:27:29 +0800

    测试CRUD第二次提交，测试版本切换

--- src/main/java/com/qiang/controller/TestGit.java
+++ src/main/java/com/qiang/controller/TestGit.java
@@ -3,5 +3,6 @@ package com.qiang.controller;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("第一次CRUD小项目");
+        System.out.println("第一次CRUD小项目");
     }
 }
